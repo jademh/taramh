c832965b41b8505697ea1435b1066125b6d8f402
jademh <jademh@gmail.com>
2018-08-24 17:12:31 +0100

    style close button of info panel

--- src/components/InfoPanel.jsx
+++ src/components/InfoPanel.jsx
@@ -27,7 +27,13 @@ class InfoPanel extends Component {
       <div className="info-panel">
         <button className={ctaClass !== '' ? ctaClass : 'info-panel_cta'} type="button" onClick={this.handleClick}>{title}</button>
         <div className={`info-panel_panel ${active ? 'st-active' : ''}`}>
-          <button className="info-panel_close" type="button" onClick={this.handleClick}>Close</button>
+          <button className="info-panel_close" type="button" onClick={this.handleClick}>
+            <svg width="48" height="48" viewPort="0 0 48 48" version="1.1"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill="red" d="M 36.019531 8.445313 L 39.558594 11.980469 L 11.980469 39.554688 L 8.445313 36.019531 Z "/>
+                <path fill="red" d="M 39.554688 36.023438 L 36.019531 39.558594 L 8.445313 11.976563 L 11.980469 8.441406 Z "/>
+            </svg>
+          </button>
           <div className="info-panel_panel-content">
             <h1>{title}</h1>
             {contentModules ? contentModules.map(edge => <Module key={edge.id} props={edge} />) : null}
